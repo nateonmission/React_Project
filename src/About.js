8f2d314abb689c7f760a3ca921e3c133c61b1105
J. Nathan Allen <nateonmission@gmail.com>
2019-05-28 19:48:00 -0400

    updated about

--- src/About.js
+++ src/About.js
@@ -9,6 +9,21 @@ const About = (props) => (
     <p>
       Welcome to [Project Name]! I designed this app as a project for CodeLouisville. Its function is Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent leo odio, lobortis eleifend consectetur non, iaculis pulvinar purus. Aenean tempus ut diam et dictum. Quisque posuere lectus a turpis fermentum, nec dictum lacus finibus. Mauris in sodales arcu. Fusce convallis quis magna sit amet molestie.
     </p>
+    <ul>
+      <li>
+        LCARS CSS FrameWork from http://joernweissenborn.github.io/lcars/
+      </li>
+      <li>
+        Star Fleet Logo made by 
+        <a href="https://www.flaticon.com/authors/those-icons" title="Those Icons">Those Icons</a>
+          from
+        <a href="https://www.flaticon.com/" 	title="Flaticon">www.flaticon.com</a>
+        is licensed by
+        <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">
+        CC 3.0 BY
+        </a>
+      </li>
+    </ul>
 
   </div>
 )
